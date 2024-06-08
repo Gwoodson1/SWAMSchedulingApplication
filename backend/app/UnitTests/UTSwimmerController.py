8c43af7fe8edc8ab0e0d193fe55ab5c899a72504
Benjamin Levesque Kinder <benjamin@Benjamins-Air.hitronhub.home>
2024-06-08 12:43:57 -0400

    This is the last commit, goodbye refactor :(

--- backend/app/UnitTests/UTSwimmerController.py
+++ backend/app/UnitTests/UTSwimmerController.py
@@ -1,13 +1,38 @@
 import pytest
+from app.run import create_app
 import app.controllers.SwimmerController as Swimmer
 
-@pytest.fixture
-def test_swimmer_data():
-    return({'id': 23, 'name': 'Khalil Omar', 'level': 3, 'special_needs': 'ADHD, ASD', 'parent_id': [45, 67]})
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    #yield app
+
+    # clean up / reset resources here
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+#OLD
+
+#def test_swimmer_data():
+#    return({'id': 23, 'name': 'Khalil Omar', 'level': 3, 'special_needs': 'ADHD, ASD', 'parent_id': [45, 67]})
 
 #tests creation of a new swimmer
-Swimmer.add_swimmer()
+#Swimmer.add_swimmer()
 
+
+#Other methods
 '''
 #tests getting information on all swimmers
 Swimmer.get_swimmers()
