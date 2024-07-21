108b14ae74915f97dbf8e258c79f17e954a93cc7
Gwoodson1 <97659671+Gwoodson1@users.noreply.github.com>
2024-07-21 11:35:08 -0400

    [Flask-api]: Changed port from 5000 to 5001

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Flask API base URL
+  baseURL: 'http://localhost:5001/api', // Your Flask API base URL
 });
 
 export default api;
